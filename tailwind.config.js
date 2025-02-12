f9257eb93739b44d398295441d7e8d4ca4ff3d82
Byukusenge Andre <andrewbyukusenge4@gmail.com>
2025-02-12 08:13:59 +0200

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
