ef3413450cdff93688716ac4b179e789dd4f87fe
Byukusenge-Andrew <andrewbyukusenge4@gmail.com>
2026-08-14 13:45:06 +0200

    feat: initialize application entry point with router, theme, and query providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeToggle } from './components/ThemeToggle.js';
 import { ThemeProvider } from './components/ThemeProvider';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -15,12 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <App />
-          {/* Theme Toggle Button (Always Visible) */}
-          <div className="fixed bottom-24 right-16  z-50"
-          title='theme toggle'
-          >
-            <ThemeToggle />
-          </div>
         </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
